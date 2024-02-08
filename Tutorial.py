ba8bdbc89a3d4ee5644b75bbeffd57a285c09f20
susanne.schmid <susanne.schmid@ucalgary.ca>
2024-02-08 14:16:56 -0700

    Created the tutorial and fixed the Code_example
    

--- Tutorial.py
+++ Tutorial.py
@@ -11,51 +11,52 @@ from scipy.ndimage import zoom
 # Load nii image using nibabel using nib.load() and store in variable img_atals
 image_name = 'icbm_avg_152_t1_tal_lin_BE.nii'
 
-img_atlas =  nib.load(image_name)
+#img_atlas=nib.load(image_name)
 
-img_data_atlas = img_atlas.get_fdata()
+#img_data_atlas = img_atlas.get_fdata()
 
 #--------------------------------
-# 2. LOAD IMAGE
+# 2. Print Dimensions of 
 #--------------------------------
 
-print(img_data_atlas.shape)
+
 
 #--------------------------------
 # 2. Image manipulation: Cropping and resizing
 #--------------------------------
 # Crop the image by removing the outer 5 pixels
 
-cropped_image = img_data_atlas[5:-5,5:-5,5:-5]
+#cropped_image = 
 
 #--------------------------------
-# 3. Resize the cropped to half its size (Zoom)
+# 3. Resize the cropped to half its size zoom((input, zoom)
 #--------------------------------
 
-resized_image = zoom(cropped_image,0.5)
+#resized_image = 
 
 
 
 #--------------------------------
-# 4. Define an ROI e.g. a 20x20x20 pixel cube
+# 4. Define an ROI on resized_image e.g. a 20x20x20 pixel cube
 #--------------------------------
 
-(x_center, y_center, z_center)=resized_image.shape
-print((x_center, y_center, z_center))
-h= 10
+#(x_center, y_center, z_center)=resized_image.shape
+#print((x_center, y_center, z_center))
+#h=
 
-roi = resized_image[x_center-h:x_center+h, y_center-h:y_center+h,z_center-h,z_center+h]
+#roi = 
 
 #--------------------------------
 # 5. Calculate mean signal intensity across the ROI for each slice
 #--------------------------------
 
-mean_intensity = np.mean(roi)
+#mean_intensity = 
 
 #--------------------------------
 print('Mean Intensity: ', mean_intensity)
 
 
+'''
 # Plot the signal intensity
 plt.plot(mean_intensity)
 plt.xlabel('Slice')
@@ -68,4 +69,4 @@ mid_slice = data[:, :, data.shape[2] // 2]
 plt.imshow(mid_slice.T, cmap='gray', origin='lower')
 plt.title('Middle Slice of an MRI Image')
 plt.show()
-
+'''
